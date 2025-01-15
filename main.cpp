baebd6ea84b45264c9d4d2179ccd9799e2adbf71
username000101 <cxx.username000101@gmail.com>
2025-01-16 01:53:36 +0300

    Calling the justmeet::logic::bot_main()

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 #include <hiredis/hiredis.h>
 
-#include "Runtime/Storage.hxx"
+#include "Logic/Bot.hxx"
 #include "Database/DBManager.hxx"
+#include "Runtime/Storage.hxx"
 
 int main() {
     using justmeet::db::DatabaseManager;
+    using justmeet::runtime_storage::bot;
     using justmeet::runtime_storage::database;
     database = std::make_shared<justmeet::db::DatabaseManager>();
+    justmeet::logic::bot_main();
     return 0;
 }
