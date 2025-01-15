c1e2e5e402203f4e2236657597d8557498b647c0
username000101 <cxx.username000101@gmail.com>
2025-01-16 01:56:00 +0300

    Added a /start command handler(initial implementation

---
+++ include/Logic/Handlers/StartCommand.hxx
@@ -0,0 +1,19 @@
+#ifndef JUSTMEET_LOGIC_HANDLERS_STARTCOMMAND_HXX
+#define JUSTMEET_LOGIC_HANDLERS_STARTCOMMAND_HXX
+
+#include <spdlog/spdlog.h>
+#include <tgbot/tgbot.h>
+
+namespace justmeet {
+    namespace logic {
+        namespace handlers {
+            namespace commands {
+                void start(TgBot::Message::Ptr message);
+                void help(TgBot::Message::Ptr message);
+                void execute(TgBot::Message::Ptr message);
+            }
+        }
+    }
+}
+
+#endif // JUSTMEET_LOGIC_HANDLERS_STARTCOMMAND_HXX
