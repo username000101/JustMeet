ab9e77013efa9f8a6aa4dc07fdc1c6c764a25eac
username000101 <cxx.username000101@gmail.com>
2025-01-16 01:54:12 +0300

    Add a new global variable: std::shared_ptr<TgBot::Bot>

--- include/Runtime/Storage.hxx
+++ include/Runtime/Storage.hxx
@@ -1,11 +1,14 @@
 #ifndef JUSTMEET_RUNTIME_STORAGE_HXX
 #define JUSTMEET_RUNTIME_STORAGE_HXX
 
-#include <Database/DBManager.hxx>
+#include <tgbot/tgbot.h>
+
+#include "Database/DBManager.hxx"
 
 namespace justmeet {
     namespace runtime_storage {
         inline std::shared_ptr<db::DatabaseManager> database;
+        inline std::shared_ptr<TgBot::Bot> bot;
     }
 }
 
