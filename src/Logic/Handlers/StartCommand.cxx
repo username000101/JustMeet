fced66d1f40517ee8a6a90ebc3b34b550e9a39a1
username000101 <cxx.username000101@gmail.com>
2025-05-02 22:48:41 +0300

    Add a support button in any case

--- src/Logic/Handlers/StartCommand.cxx
+++ src/Logic/Handlers/StartCommand.cxx
@@ -20,25 +20,25 @@ void justmeet::logic::handlers::commands::start(TgBot::Message::Ptr message) {
 
     auto keyboard = std::make_shared<TgBot::InlineKeyboardMarkup>();
 
-    if (!database->check_user(message->from->id)) {
-        auto button_support = std::make_shared<TgBot::InlineKeyboardButton>();
-        button_support->text = "Поддержка";
-        button_support->url = "https://t.me/meetx_discussion";
+    auto button_support = std::make_shared<TgBot::InlineKeyboardButton>();
+    button_support->text = "Поддержка";
+    button_support->url = "https://t.me/meetx_discussion";
+    keyboard->inlineKeyboard.push_back({button_support});
 
+    if (!database->check_user(message->from->id)) {
         auto button_create_profile = std::make_shared<TgBot::InlineKeyboardButton>();
         button_create_profile->text = "Создать профиль";
-        button_create_profile->callbackData = query::QRY_LOG + " unimplemented function called";
+        button_create_profile->callbackData = query::QRY_WARN + " this part is not implemented now :(";
 
         keyboard->inlineKeyboard.push_back({button_create_profile});
-        keyboard->inlineKeyboard.push_back({button_support});
         bot->getApi().sendMessage(message->chat->id, "Привет, " + message->from->firstName + "! Эта часть ещё в разработке...", nullptr, reply_parameters, keyboard);
     } else {
         auto user = database->get_user(message->from->id);
         if (!user.has_value())
-            bot->getApi().sendMessage(message->chat->id, "Привет, незвестный пользователь(вероятно, ошибка redis)! Эта часть ещё в разработке...", nullptr, reply_parameters);
+            bot->getApi().sendMessage(message->chat->id, "Привет, незвестный пользователь(вероятно, ошибка redis)! Эта часть ещё в разработке...", nullptr, reply_parameters, keyboard);
         else {
-            auto user_value = user.value();
-            bot->getApi().sendMessage(message->chat->id, "Привет, " + (user_value.name.empty() ? "пользователь без имени" : user_value.name) + "! Эта часть ещё в разработке...", nullptr, reply_parameters);
+            const auto& user_value = user.value();
+            bot->getApi().sendMessage(message->chat->id, "Привет, " + (user_value.name.empty() ? "пользователь без имени" : user_value.name) + "! Эта часть ещё в разработке...", nullptr, reply_parameters, keyboard);
         }
     }
 }
