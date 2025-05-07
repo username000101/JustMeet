4b5f64cfc11107d669c03b550d62ee47af578b0c
username000101 <cxx.username000101@gmail.com>
2025-05-07 23:04:39 +0300

    Check the value of redisReply::integer

--- src/Database/CheckField.cxx
+++ src/Database/CheckField.cxx
@@ -12,7 +12,7 @@ bool justmeet::db::DatabaseManager::check_field(std::int64_t chat_id, const std:
                                             std::to_string(chat_id).c_str(),
                                             field.c_str());
 
-    if (reply->type == REDIS_REPLY_NIL)
+    if (reply->type == REDIS_REPLY_NIL || (reply->type ==  REDIS_REPLY_INTEGER && reply->integer == 0))
         return false;
     else
         return true;
