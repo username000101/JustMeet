d8790811eefcebeaf38fb6d9f610aa763b5d9123
username000101 <cxx.username000101@gmail.com>
2025-05-07 23:05:26 +0300

    The function has been completely rewritten and now works correctly

--- src/Database/GetField.cxx
+++ src/Database/GetField.cxx
@@ -3,6 +3,7 @@
 #include <cstdint>
 
 #include <hiredis/hiredis.h>
+#include <unicode/urename.h>
 
 std::optional<std::string> justmeet::db::DatabaseManager::get_field(std::int64_t chat_id, const std::string& field) {
     const char* format = "hget %s %s";
@@ -10,12 +11,52 @@ std::optional<std::string> justmeet::db::DatabaseManager::get_field(std::int64_t
                                            format,
                                            std::to_string(chat_id).c_str(),
                                              field.c_str());
-    if (reply->type == REDIS_REPLY_NIL)
+    if (reply->type == REDIS_REPLY_NIL || reply->type == REDIS_REPLY_ERROR)
         return std::nullopt;
     else {
-        if (reply->element[0] && std::strcmp(reply->element[0]->str, field.c_str()) == 0 && reply->element[1])
-            return reply->element[1]->str;
-        else
-            return std::nullopt;
+        switch (reply->type) {
+            case REDIS_REPLY_STRING:
+                return (reply->str ? reply->str : "char* == nullptr");
+            case REDIS_REPLY_INTEGER:
+                return std::to_string(reply->integer);
+            case REDIS_REPLY_ARRAY: {
+                std::string array_str;
+                for (auto element = 0; element != reply->elements; ++element)
+                    if (reply->element[element] && reply->element[element + 1]) {
+                        array_str.append("[");
+                        switch (reply->element[element]->type) {
+                            case REDIS_REPLY_STRING:
+                                array_str.append(std::string(reply->element[element]->str));
+                                break;
+                            case REDIS_REPLY_INTEGER:
+                                array_str.append(std::to_string(reply->element[element]->integer));
+                                break;
+                            default:
+                                array_str.append("UNKNOWN");
+                                break;
+                        }
+                        array_str.append("](");
+
+                        switch (reply->element[element + 1]->type) {
+                            case REDIS_REPLY_STRING:
+                                array_str.append(std::string(reply->element[element + 1]->str));
+                                break;
+                            case REDIS_REPLY_INTEGER:
+                                array_str.append(std::to_string(reply->element[element + 1]->integer));
+                                break;
+                            default:
+                                array_str.append("UNKNOWN");
+                        }
+                        array_str.append(")");
+                    }
+                return array_str;
+            }
+            case REDIS_REPLY_NIL:
+                return std::nullopt;
+            case REDIS_REPLY_ERROR:
+                return std::nullopt;
+            default:
+                return std::nullopt;
+        }
     }
 }
