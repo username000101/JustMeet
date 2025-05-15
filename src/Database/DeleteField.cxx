4f52b2934bc446e0ff71187ead0f79c50d505db9
username000101 <cxx.username000101@gmail.com>
2025-05-15 20:33:23 +0300

    1. Add more logging 2. Use "user:<chat_id>" instead "<chat_id>" for the key

--- src/Database/DeleteField.cxx
+++ src/Database/DeleteField.cxx
@@ -2,17 +2,15 @@
 
 bool justmeet::db::DatabaseManager::delete_field(std::int64_t chat_id, const std::string& name) {
     auto reply = (redisReply*)redisCommand(this->redis_,
-                                           "hdel %s %s",
+                                           "hdel user:%s %s",
                                            std::to_string(chat_id).c_str(),
                                            name.c_str());
     if (reply->type == REDIS_REPLY_INTEGER) {
-        this->logger_->log(spdlog::level::info,
-                           "{} ==> Field of key {} has been deleted",
+        this->logger_->info("{} ==> Field of key {} has been deleted",
                            __PRETTY_FUNCTION__, chat_id);
         return true;
     } else {
-        this->logger_->log(spdlog::level::warn,
-                           "{} ==> Failed to delete field of key {}: redisReply::type is {}",
+        this->logger_->warn("{} ==> Failed to delete field of key {}: redisReply::type is {}",
                            __PRETTY_FUNCTION__, chat_id, reply->type);
         return false;
     }
