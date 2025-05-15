6416fd2a63fc6322cde0144c45cb983ac7b65242
username000101 <cxx.username000101@gmail.com>
2025-05-15 20:32:31 +0300

    Use "user:<chat_id>" instead "<chat_id>" for the key

--- src/Database/AddField.cxx
+++ src/Database/AddField.cxx
@@ -3,7 +3,7 @@
 bool justmeet::db::DatabaseManager::add_field(std::int64_t chat_id, const std::string& name, const std::string& value) {
 
     auto reply = (redisReply*)redisCommand(this->redis_,
-                                           "hset %s %s %s",
+                                           "hset user:%s %s %s",
                                              std::to_string(chat_id).c_str(),
                                              name.c_str(),
                                              value.c_str());
